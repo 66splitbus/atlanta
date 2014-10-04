21c4240819983d166510a860f83808da8e85d7aa
Mark <marks@unity3d.com>
2014-10-04 11:55:53 -0700

    lets see if this works

---
+++ Assets/Scripts/Pickup2.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pickup2 : MonoBehaviour
+{
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		// If the player hits the trigger...
+		if(col.gameObject.tag == "Player")
+		
+		{
+			rigidbody2D.velocity = new Vector2( rigidbody2D.velocity.y , 100  );
+		
+		}
+
+	}
+}
---
+++ Assets/Scripts/PickupScore.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupScore : MonoBehaviour
+{
+		
+	public float score = 0f;
+	public GUIText guiText;
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		// If the player hits the trigger...
+		if(col.gameObject.tag == "Player")
+		
+		{
+			Destroy (gameObject);	
+			score++;
+			guiText.text = "Points: " + score + "/8";
+		}
+
+	}
+}
